1cb589b57e1f55896377191c787d2de3b31147b5
sunjianfeng <sunjianfeng911@163.com>
2020-06-23 14:23:26 +0800

    first commit

---
+++ sys/src/test/java/com/cloud/alibaba/sys/SysApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cloud.alibaba.sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
